6edb6c7d0c76794689d4f24e2345b01195a58e26
mkohli3 <mkohli3@asu.edu>
2022-10-27 22:32:38 -0700

    Made changes to ifFileEmpty() and test passes

--- src/com/company/urinalsFunctions.java
+++ src/com/company/urinalsFunctions.java
@@ -51,9 +51,9 @@ public class urinalsFunctions {
     }
     public static int ifFileEmpty(String line){
         if(line==null){
-            return 0;
+            return 1;
         }
-        return 1;
+        return 0;
     }
     public static int readFile(String line){
         if(line==null){
